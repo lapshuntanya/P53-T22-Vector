3a75642cbd5faeea234e80cfe5ce798614736a5a
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-07-01 17:24:13 +0300

    Vector

---
+++ myVector.h
@@ -0,0 +1,74 @@
+//
+// Created by Tetiana Perederii on 01.07.2026.
+//
+
+#ifndef P53_T22_VECTOR_MYVECTOR_H
+#define P53_T22_VECTOR_MYVECTOR_H
+#include <iostream>
+using namespace std;
+
+template<typename Type>
+class Vector {
+    Type* arr;
+    int size;
+
+public:
+    Vector() {
+        arr = nullptr;
+        size = 0;
+    }
+    Vector(int size, Type value) { //5, 1 -> [1, 1, 1, 1, 1]
+        if ( size > 0) {
+            this->size = size;
+            arr = new  Type[size];
+            for (int i = 0; i < size; ++i) {
+                arr[i] = value;
+            }
+        }else {
+            arr = nullptr;
+            size = 0;
+        }
+    }
+    ~Vector() {
+        if (arr!= nullptr) {
+            delete[] arr;
+
+            arr = nullptr;
+            size = 0;
+        }
+    }
+
+    int getSize() const	// повертає розмірність масиву
+    {
+        return size;
+    }
+    void print()const	// виведення всіх елементів масиву
+    {
+        if ( size == 0 ) cout << "Vector is empty!\n" ;
+        else {
+            for (int i = 0; i < size; ++i) {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void push_back(Type item)	// додавання елемента item у кінець масиву
+    {
+        Type* tmp = new Type[++size];
+        for (int i = 0; i < size - 1; ++i) {
+            tmp[i] = arr[i];
+        }
+        tmp[size - 1] = item;
+
+        if (arr!= nullptr) delete[] arr;
+
+        arr = tmp;
+    }
+    Type& operator[](int number);
+
+    Type find_max()const; // пошук елемента з максимальним значенням
+    Type find_min()const; // пошук елемента з мінімальним значенням
+
+};
+#endif //P53_T22_VECTOR_MYVECTOR_H
